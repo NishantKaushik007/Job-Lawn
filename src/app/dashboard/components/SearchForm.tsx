23fb5895e29af2341ac155d3772bac028adc57f9
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-02-22 17:37:36 +0530

    Search button cursor made pointer

--- src/app/dashboard/components/SearchForm.tsx
+++ src/app/dashboard/components/SearchForm.tsx
@@ -59,7 +59,7 @@ const SearchForm: React.FC<SearchFormProps> = ({ initialKeyword }) => {
       />
       <button
         type="submit"
-        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 h-10"
+        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 h-10 cursor-pointer"
         disabled={!keyword}
       >
         Search
